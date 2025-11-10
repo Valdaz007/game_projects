bc84d44d9312e5fd225d64ba1703de820af94eb6
Victor <volsavaivictor@gmail.com>
2025-11-10 19:48:44 +1000

    Update: Main App Files & Touch Ups

--- diceApp.js
+++ diceApp.js
@@ -1,7 +1,40 @@
+const one = "rotate3d(1, 0, 0, 180deg)"
+const two = "rotate3d(1, 0, 0, 0deg)"
+const three = "rotate3d(0, 1, 0, -90deg)"
+const four = "rotate3d(0, 1, 0, 90deg)"
+const five = "rotate3d(1, 0, 0, -90deg)"
+const six = "rotate3d(1, 0, 0, 90deg)"
+
+let startSide = one
+let endSide = ""
+let dice = document.getElementById('dice');
+
 function roll(){
-    x = Math.floor(Math.random() * (5 - 0 + 1)) + 0;
-    x = x*100;
-    document.querySelector('svg').viewBox.baseVal.x = x;
+    x = Math.floor(Math.random() * (6 - 1 + 1)) + 1
+    rollAnimation(x)
 }
 
-roll()
+function rollAnimation(ranNum){
+    if(ranNum == 1) endSide = one
+    if(ranNum == 2) endSide = two
+    if(ranNum == 3) endSide = three
+    if(ranNum == 4) endSide = four
+    if(ranNum == 5) endSide = five
+    if(ranNum == 6) endSide = six
+
+    dice.animate(
+        [
+            { transform: startSide },
+            { transform: "rotateX(720deg) rotateY(720deg)" },
+            { transform: "rotateX(-540deg) rotateY(540deg)" },
+            { transform: endSide }
+        ],
+        {
+            duration: 3500,
+            easing: "ease-in-out",
+            fill: "forwards"
+        }
+    )
+
+    startSide = endSide
+}
