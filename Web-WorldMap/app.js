adc8ea7fbe2d7085dd21cbefd3a6fa4ad8da1cf6
Victor <volsavaivictor@gmail.com>
2025-12-21 13:51:15 +1000

    Update: Game Play Template

--- Web-WorldMap/app.js
+++ Web-WorldMap/app.js
@@ -1,4 +1,4 @@
-countryCode = `[{"AD":"Andorra"},{"AE":"United Arab Emirates"},{"AF":"Afghnistan"},{"AG":"Antigua & Barbuda"},
+countryCodeOG = `[{"AD":"Andorra"},{"AE":"United Arab Emirates"},{"AF":"Afghnistan"},{"AG":"Antigua & Barbuda"},
                 {"AI":"Anguilla"},{"AL":"Albanida"},{"AM":"Armenia"},{"AN":"Netherland Antilles"},{"AO":"Angola"},
                 {"AQ":"Antarctica"},{"AR":"Argentina"},{"AS":"America Somoa"},{"AT":"Austria"},{"AU":"Australia"},
                 {"AW":"Aruba"},{"AX":"Aland Islands"},{"AZ":"Azerbaijan"},{"BA": "Bosnia and Herzegovina"},
@@ -10,16 +10,41 @@ countryCode = `[{"AD":"Andorra"},{"AE":"United Arab Emirates"},{"AF":"Afghnistan
                 {"CH": "Switzerland"},{"CI": "Cote Dloire"},{"CK": "Cook Islands"},{"CL": "Chile"},{"CM": "Cameroon"},
                 {"CN": "China"},{"CO": "Colombia"},{"CR": "Costa Rica"},{"CU": "Cuba"},{"CV": "Cape Verde"},{"CW": "Curacoa"},
                 {"CX": "Christmas Island"},{"CY": "Cyprus"},{"CZ": "Czech Republic"}]`
-countryCode = JSON.parse(countryCode)
+countryCodeOG = JSON.parse(countryCodeOG);
+gameResult = [] //{'correct':'', 'answer':''}
 
-// countryCode.forEach((i)=>{
-//     $('.flags').append(`
-//         <img src='https://flagsapi.com/${Object.keys(i).join('')}/flat/64.png'>
-//     `)
-// })
+
+function shuffleCountry(array){
+    for(let i = array.length-1; i > 0; i--){
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array
+}
+
+function checkUsername(){
+    return $('.uname').val()==''
+}
 
 $('#flagQz').click(function(e){
-    alert('Feature Still Under Developement!')
+    if(checkUsername()){
+        alert('Please Enter Username to Continue!')
+    }
+    else {
+        gameCode = shuffleCountry(countryCodeOG);
+        console.log(Object.keys(gameCode[0]).join(''))
+        $('body').empty()
+        $('body').append('<temp-play></temp-play>')
+        $('.playflag').append(`
+            <img src='https://flagsapi.com/${Object.keys(gameCode[0]).join('')}/flat/64.png'>
+        `);
+        let choiceArr = gameCode.slice(0, 4);
+        console.log(choiceArr[0][Object.keys(choiceArr[0]).join('')])
+        choiceArr = shuffleCountry(choiceArr)
+        choiceArr.forEach((i, idx) => {
+            $(`#chc${idx+1}`).text(`${i[Object.keys(i).join('')]}`)
+        });
+    }
 })
 
 $('#shapeQz').click(function(e){
--- Web-WorldMap/temp/tempMngr.js
+++ Web-WorldMap/temp/tempMngr.js
@@ -45,4 +45,39 @@ class TempIndex extends HTMLElement {
     }
 }
 
+class PlayTemp extends HTMLElement {
+
+    connectedCallback() {
+        this.innerHTML = `
+            <h1>WORLDMAPQuiz Game</h1>
+            <div class='info'>
+                <h5>How far can you go guessing the Countries Flag?</h5>
+                <h5>Game ends when guessed wrong or all flags are finished.</h5>   
+            </div>
+            <div class='mainplay'>
+                <div class='playflag'></div>
+                <div class='playChoices'>
+                    <div class='choice'>
+                        <p class='lbl'>A</p>
+                        <p id='chc1'>Choice 1</p>
+                    </div>
+                    <div class='choice'>
+                        <p class='lbl'>B</p>
+                        <p id='chc2'>Choice 2</p>
+                    </div>
+                    <div class='choice'>
+                        <p class='lbl'>C</p>
+                        <p id='chc3'>Choice 3</p>
+                    </div>
+                    <div class='choice'>
+                        <p class='lbl'>D</p>
+                        <p id='chc4'>Choice 4</p>
+                    </div>
+                </div>
+            </div>
+        `
+    }
+}
+
 customElements.define('temp-index', TempIndex)
+customElements.define('temp-play', PlayTemp)
