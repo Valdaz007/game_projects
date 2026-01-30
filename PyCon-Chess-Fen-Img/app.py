aace2b3f7c84c10cf3733f1fec4952222d015f20
victor-home-pc <volsavaivictor@gmail.com>
2026-01-30 16:30:51 +1000

    Update: PCFI - Draw a Square Func

--- PyCon-Chess-Fen-Img/app.py
+++ PyCon-Chess-Fen-Img/app.py
@@ -1,7 +1,21 @@
 import turtle
 
-turtle.setup(width=606, height=600)
+def square(t, len:int):
+    t.color('red')
+    t.begin_fill()
+    for _ in range(4):
+        t.forward(len)
+        t.right(90)
+    t.end_fill()
 
-ttl = turtle.Turtle()
+def main():
+    turtle.setup(width=606, height=600)
+    turtle.title('MiChessFEN-IMG')
 
-turtle.done()
+    ttl = turtle.Turtle()
+    square(ttl, 100)
+
+    turtle.done()
+
+if __name__ == '__main__':
+    main()
