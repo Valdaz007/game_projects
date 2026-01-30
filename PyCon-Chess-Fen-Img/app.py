7e43600d6a0679e18fef20ca125db73a675849d6
victor-home-pc <volsavaivictor@gmail.com>
2026-01-30 16:40:23 +1000

    Update: PCFI - Draw Rank

--- PyCon-Chess-Fen-Img/app.py
+++ PyCon-Chess-Fen-Img/app.py
@@ -1,19 +1,35 @@
 import turtle
 
 def square(t, len:int):
-    t.color('red')
     t.begin_fill()
     for _ in range(4):
         t.forward(len)
         t.right(90)
+    t.forward(len)
     t.end_fill()
+
+def rank(t, x, y, c1, c2):
+    t.setx(x)
+    t.sety(y)
+
+    t.pendown()
+
+    for _ in range(4):
+        t.color(c1)
+        square(t, 75)
+        t.color(c2)
+        square(t, 75)
 
 def main():
     turtle.setup(width=606, height=600)
     turtle.title('MiChessFEN-IMG')
 
     ttl = turtle.Turtle()
-    square(ttl, 100)
+    ttl.hideturtle()
+    ttl.pensize(0)
+    ttl.penup()
+
+    rank(ttl, -300, 300, '#D18B47', '#FFCE9E')
 
     turtle.done()
 
