86973b91cb0c304e67bb4ee7d9177094aa540b1e
victor-home-pc <volsavaivictor@gmail.com>
2026-01-30 16:48:19 +1000

    Update: PCFI - Draw Chess Board

--- PyCon-Chess-Fen-Img/app.py
+++ PyCon-Chess-Fen-Img/app.py
@@ -8,7 +8,7 @@ def square(t, len:int):
     t.forward(len)
     t.end_fill()
 
-def rank(t, x, y, c1, c2):
+def rank(t, x, y, c1='#D18B47', c2='#FFCE9E'):
     t.setx(x)
     t.sety(y)
 
@@ -20,16 +20,25 @@ def rank(t, x, y, c1, c2):
         t.color(c2)
         square(t, 75)
 
+def board(t, x, y):
+    for _ in range(4):
+        rank(t, x, y)
+        y-=75
+        rank(t, x, y, '#FFCE9E', '#D18B47')
+        y-=75
+
 def main():
     turtle.setup(width=606, height=600)
     turtle.title('MiChessFEN-IMG')
 
     ttl = turtle.Turtle()
     ttl.hideturtle()
+    ttl.speed(10)
     ttl.pensize(0)
     ttl.penup()
 
-    rank(ttl, -300, 300, '#D18B47', '#FFCE9E')
+    # rank(ttl, -300, 300)
+    board(ttl, -300, 300)
 
     turtle.done()
 
